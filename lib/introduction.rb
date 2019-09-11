3d501bb747882fbc54c4022d01a37065c323744a
Abraham Moreno <adiaz2821@gmail.com>
2019-09-11 14:08:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,13 @@
-def introduction (name = "Juan")
+def introduction(name)
   puts "Hi, my name is #{name}."
+end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learing to program in #{language}."
 
 
 
